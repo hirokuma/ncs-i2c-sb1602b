82c652ca38a9ab9ea2b7a2390448e59dc44df161
hirokuma <hiro9ma@gmail.com>
2024-09-10 11:59:06 +0900

    整理
    * LED3つとも初期化時にOFF
    * overlayサンプルの見直し
    * 起動時に一度LCDがクリアされた状態を作る

--- src/drivers/led.c
+++ src/drivers/led.c
@@ -4,6 +4,8 @@
 #include "led.h"
 
 #define LED0_NODE               DT_ALIAS(led0)
+#define LED1_NODE               DT_ALIAS(led1)
+#define LED2_NODE               DT_ALIAS(led2)
 
 static const struct gpio_dt_spec dev_led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
@@ -17,6 +19,10 @@ int led_init(void)
 	if (ret < 0) {
 		return -1;
 	}
+	struct gpio_dt_spec dev_led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+	struct gpio_dt_spec dev_led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
+	gpio_pin_configure_dt(&dev_led1, GPIO_OUTPUT_INACTIVE);
+	gpio_pin_configure_dt(&dev_led2, GPIO_OUTPUT_INACTIVE);
 
     return 0;
 }
--- src/main.c
+++ src/main.c
@@ -4,6 +4,22 @@
 #include "drivers/led.h"
 #include "drivers/sb1602b.h"
 
+#if defined CONFIG_BOARD_SSCI086585_NRF5340_CPUAPP
+
+#define MESSAGE "Hello,nRF5340!"
+
+#elif defined CONFIG_BOARD_SSCI086585_NRF5340_CPUAPP_NS
+
+#define MESSAGE "Hello,nRF5340ns!"
+
+#else
+
+#error "This configuration not supported."
+
+#endif
+
+static void startup(void);
+
 int main(void)
 {
     int ret;
@@ -14,21 +30,27 @@ int main(void)
         return -1;
     }
 
-    for (int lp = 0; lp < 3; lp++) {
-        led_set(1);
-        k_msleep(1000);
-        led_set(0);
-        k_msleep(1000);
-    }
+    startup();
 
     ret = sb1602b_init();
     if (ret != 0) {
         printk("fail sb1602b_init(): ret=%d\n", ret);
         return -1;
     }
+    k_msleep(1000); // LCDが消えている状態を目視したいので待たせる
 
-    sb1602b_print("Hello, World!");
+    sb1602b_print(MESSAGE);
     printk("done.\n");
 
     return 0;
+}
+
+static void startup(void)
+{
+    for (int lp = 0; lp < 3; lp++) {
+        led_set(1);
+        k_msleep(500);
+        led_set(0);
+        k_msleep(500);
+    }
 }
